6b5ee7ac7ca9eef47bb5a7501bea5f546f4dd72c
Koo <manuelsaniez02@gmail.com>
2022-11-01 00:17:36 -0300

    Followed yt tutorial to start using js

---
+++ script.js
@@ -0,0 +1,6 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButtons = document.querySelector('[data-equals]')
+const equalsButtons = document.querySelector('[data-equals]')
+const equalsButtons = document.querySelector('[data-equals]')
+const equalsButtons = document.querySelector('[data-equals]')
